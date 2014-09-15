29e8f201432b3ab1e50894c4756bb63f665b0a89
Kim Jung-Woo <kmiracle@gmail.com>
2014-09-15 13:53:24 +0900

    copyright 제거.

--- src/main/java/ch01/Example.java
+++ src/main/java/ch01/Example.java
@@ -1,10 +1,3 @@
-/*
- * @(#)Example.class  2014. 9. 12..
- *
- * Copyright 2007 NHN Corp. All rights Reserved. 
- * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
-
 package ch01;
 
 import javafx.scene.control.Button;
